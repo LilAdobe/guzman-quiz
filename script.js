bfaf02470a54f4bdfa40602bdbb57e67d0d8783b
Alexis Guzman <alexisguzman157@outlook.com>
2021-08-26 22:38:29 -0400

    made the timer work

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-var timerEl = document.querySelector
-var startEl = document.querySelector
-var startBtn = document.querySelector
-
+var timerEl = document.querySelector('#timer');
+var startEl = document.querySelector('#start');
+var startBtn = document.querySelector('#start-btn');
+var quizEl = document.querySelector('#quiz');
+var endEl = document.querySelector('#end');
 
 var timeLeft = 60;
 
+var startTimer = function () {
+    var interval = setInterval(function () {
+        timeLeft--;
+        timerEl.textContent = timeLeft;
+        if (timeLeft <= 0) {
+            timeLeft = 0;
+            clearInterval(interval);
+        }
+    }, 1000);
+}
+
+
+
+startBtn.addEventListener('click', function () {
+    startTimer();
+    showQuiz();
+})
