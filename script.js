3ebd6b6099f0a762f1877a3875f0f0474d556e21
Alexis Guzman <alexisguzman157@outlook.com>
2021-08-26 21:29:45 -0400

    added a start button and set up the timer element, currently not working

--- script.js
+++ script.js
@@ -1,32 +1,7 @@
-var quizContainer = document.getElementById('#quiz');
-var resultsContainer = document.getElementById('#results');
-var submitButton = document.getElementById('#submit');
+var timerEl = document.querySelector
+var startEl = document.querySelector
+var startBtn = document.querySelector
 
 
+var timeLeft = 60;
 
-
-generateQuiz(quizQuestion, quizContainer, resultsContainer, submitButton);
-
-
-
-
-var quizQuestion = [
-    {
-        question: "1+2=x",
-        answers: {
-            a: '1',
-            b: '2',
-            c: '3'
-        },
-        correctAnswer: 'c'
-    },
-    {
-        question: " x+2=4 ?"
-        answers: {
-            a:'2',
-            b:'4',
-            c:'5'
-        }
-        correctAnswer: 'a'
-    }
-];
